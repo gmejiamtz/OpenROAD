60b62bd88ec707f1c744d763bf90461e1bff2b9b
gmejiamtz <gmejiama@ucsc.edu>
2025-04-22 17:17:03 -0700

    Added CMS to logger.h foreachtool

--- src/utl/include/utl/Logger.h
+++ src/utl/include/utl/Logger.h
@@ -37,6 +37,7 @@ class Progress;
 // Keep this sorted
 #define FOREACH_TOOL(X) \
   X(ANT)                \
+  X(CMS)                \
   X(CTS)                \
   X(DFT)                \
   X(DPL)                \

c050e17c51683100c36fd3bc4481e69e0b87e854
Gary Mejia <gmejiama@ucsc.edu>
2025-06-27 16:31:16 -0700

    Using the assignment operator to copy over straps points vector to points_

--- src/cms/src/ClockMesh.cc
+++ src/cms/src/ClockMesh.cc
@@ -196,10 +196,10 @@ ClockMesh::makeGrid()
   }
 
   Straps straps_(layer, 0, 0);
-  std::vector<odb::Point> strap_points = straps_.makeStraps(0, 0, 0, 0, 0, 0, true, obs_tree);
-  for (int i = 0; i < strap_points.size(); i++) {
-    points_[i] = std::copy(strap_points[i]);
-  }
+  points_ = straps_.makeStraps(0, 0, 0, 0, 0, 0, true, obs_tree);
+  //for (int i = 0; i < strap_points.size(); i++) {
+  //  points_[i] = std::copy(strap_points[i]);
+  //}
   this->strap_count_ += points_.size();
 }
 
